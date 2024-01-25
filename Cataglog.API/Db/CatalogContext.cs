d305cd35137d3496339e9bdbe85cda54956cfa4c
Jakka Divya Sree <jaksree@deloitte.com>
2024-01-25 19:03:05 +0530

    Add project files.

---
+++ Cataglog.API/Db/CatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Cataglog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cataglog.API.Db
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+    }
+}
